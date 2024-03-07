394009d94f165122a0c3c2a2156b6b279ed7c5d2
Verracciii <valterssocial@gmail.com>
2024-03-07 09:31:52 +0000

    Refactor workout template styles

--- StrengoApp/app/workout/[template].jsx
+++ StrengoApp/app/workout/[template].jsx
@@ -15,20 +15,80 @@ const styles = StyleSheet.create({
         backgroundColor: '#e1e4f5',
         padding: 20,
         width: '100%',
+        flexWrap: 'wrap',
         text: {
+
             color: 'black',
             fontSize: 20,
             fontWeight: 'bold'
-        }
-    },
-    workoutTitle: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        color: 'white',
-        paddingLeft: 25,
-    },
-    workoutLog: {
 
+        },
+    },
+    workout: {
+        workoutBox: {
+
+            backgroundColor: 'lightgrey',
+            width: '100%',
+            height: 'auto',
+            flexDirection: 'column',
+            alignItems: 'center',
+
+            workoutTitleBox: {
+
+                backgroundColor: '#3f51b5',
+                flexDirection: 'row',
+                width: '100%',
+
+                workoutTitle: {
+
+                    fontSize: 20,
+                    fontWeight: 'bold',
+                    color: 'white',
+                    paddingLeft: 25,
+
+                },
+            },
+            workoutRow:{
+
+                width: '100%',
+                flexDirection: 'row',
+                justifyContent: 'space-around',
+                alignItems: 'center',
+                padding: 15,
+
+                workoutNo: {
+                    
+                    workoutNoText:{
+                        fontSize: 20,
+                        fontWeight: 'bold',
+                    
+                    },
+                },
+                workoutInput: {
+
+                    backgroundColor: 'grey',
+                    width: '10%',
+                    height: '95%',
+
+                },
+                workoutFinishButton: {
+
+                    backgroundColor: 'green',
+                    width: '20%',
+                    height: '95%',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+
+                    workoutFinishButtonText: {
+
+                        color: 'white',
+                        fontWeight: 'bold',
+                        fontSize: 17,
+
+                    },
+                },
+            }
+        }
     }
 })
 
@@ -36,28 +96,44 @@ const styles = StyleSheet.create({
 const WorkoutRow = (template, templateId) => {
         
         return (
-            pass
+            <View style={styles.workout.workoutBox.workoutRow}>
+                <View style={styles.workout.workoutBox.workoutRow.workoutNo}>
+                    <Text style={styles.workout.workoutBox.workoutRow.workoutNo.workoutNoText}>1</Text>
+                </View>
+                <View style={styles.workout.workoutBox.workoutRow.workoutInput}>
+
+                </View>
+                <View style={styles.workout.workoutBox.workoutRow.workoutInput}>
+                    
+                </View>
+                <View style={styles.workout.workoutBox.workoutRow.workoutFinishButton}>
+                    <Text style={styles.workout.workoutBox.workoutRow.workoutFinishButton.workoutFinishButtonText}>Finish</Text>
+                </View>
+
+            </View>
         )
     }
 
 const Workout = (template, templateId) => {
     
     return (
-        pass
+        <View>
+            <WorkoutRow template={template} templateiD={templateId}/>
+        </View>
     )
 }
 
 const WorkoutList = (template, templateId) => {
 
     return (
-        <View style={{
-            backgroundColor: 'red',
-            width: '100%'
-            }}>
+        <ScrollView style={{
+            flexDirection: 'column',
+        }}>
 
-            <Text style={styles.workoutTitle}>Workout</Text>
+            <Workout template={template} templateId={templateId} />
 
-        </View>
+        </ScrollView>
+
     )
 }
 
@@ -85,11 +161,29 @@ export default function newWorkout() {
 
         <View>
             <View style={styles.topBar}>
-                <Text style={styles.topBar.text}>Time elapsed: </Text>
+                <View style={{
+                    width: '100%',
+                    alignItems: 'center',
+                }}>
+                    <Text style={styles.topBar.text}>Time elapsed: </Text>
+                </View>
+
+                <View style={{
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    paddingLeft: '15%',
+                    paddingRight: '30%',
+                    width:'100%',
+                    alignItems: 'center',
+                    }}>
+                    <Text>Reps</Text>
+                    <Text>Sets</Text>
+                </View>
+
             </View>
 
-            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'flex-start', alignItems: 'center' }}>
-                <WorkoutList />
+            <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: 'flex-start', alignItems: 'center', flexDirection: "column" }}>
+                <WorkoutList template={template} templateId={templateId}/>
             </ScrollView>
         </View>
     )
